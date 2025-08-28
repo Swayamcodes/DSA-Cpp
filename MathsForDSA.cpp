65d9c1219f065b8910c008d19ea159ff8cdbca58
Swayam Shinde <shindeswayam2004@gmail.com>
2025-08-28 13:36:42 +0530

    `Added reverse integer function for LeetCode 7`

--- MathsForDSA.cpp
+++ MathsForDSA.cpp
@@ -90,3 +90,20 @@ int main () {
     printDigits(n);
     return 0;
 }
+
+// Reverse Integer Leetcode 7
+// class Solution {
+// public:
+//     int reverse(int n) {
+//         int revNum = 0;
+//         while ( n!= 0 ) {
+//             int dig = n%10;
+//             if (revNum > INT_MAX/10 || revNum < INT_MIN/10) {
+//                 return 0;
+//             }
+//             revNum = revNum*10 + dig;
+//             n = n/10; 
+//         }
+//         return revNum;
+//     }
+// };
