8a775bb4ed651b2eda79e9bcc219f74ba01eeb04
Swayam Shinde <shindeswayam2004@gmail.com>
2025-08-28 12:43:22 +0530

    `Added function to check if a number is an Armstrong number and updated main function to test it`

--- MathsForDSA.cpp
+++ MathsForDSA.cpp
@@ -45,12 +45,24 @@ void printDigits(int n) {
     int count = 0;
     while (n != 0)
     {
-        int digit = n % 10;
+        int digit = n % 10; 
         count++;
         cout << digit << endl;
         n = n / 10;
     }
     cout << count << endl;
+}
+
+bool isArmstrong(int n) {
+    int copyN = n;
+    int sumOfCubes = 0;
+
+    while (n != 0) {
+        int dig = n % 10;
+        sumOfCubes += (dig * dig * dig);
+        n = n / 10;
+    }
+    return sumOfCubes == copyN;
 }
 
 int main () {
