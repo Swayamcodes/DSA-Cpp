55c44f16dff6444fe61f10963c787d2a7f5f3eff
Swayam Shinde <shindeswayam2004@gmail.com>
2025-08-28 13:42:40 +0530

    `Added implementation for LeetCode problem 9: Palindrome Number`

--- MathsForDSA.cpp
+++ MathsForDSA.cpp
@@ -107,3 +107,22 @@ int main () {
 //         return revNum;
 //     }
 // };
+
+// Palindrome Number Leetcode 9
+// class Solution {
+// public: 
+//     bool isPalindrome(int x) {
+//         if (x < 0) return false;
+//         int original = x;
+//         int revNum = 0; 
+//         while ( x!= 0 ) {
+//             int dig = x%10;
+//             if (revNum > INT_MAX/10 || revNum < INT_MIN/10) {
+//                 return false;            
+//             }
+//             revNum = revNum*10 + dig;
+//             x = x/10;
+//         }
+//         return revNum == original;
+//     }
+// };
