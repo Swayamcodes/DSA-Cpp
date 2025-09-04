6e5597510fb36b1f80f11d73ebdac0edcc851353
Swayam Shinde <shindeswayam2004@gmail.com>
2025-09-04 14:14:00 +0530

    Add isSorted function to check if an array is sorted

--- 42.Recursion.cpp
+++ 42.Recursion.cpp
@@ -25,11 +25,21 @@ int Sum(int n) {
         return n + Sum(n-1);
 }
 
+bool isSorted(int arr[], int n) {
+    //base case
+    if ( n == 0 || n == 1 ) 
+        return true;
+        
+        return arr[n-1] > arr[n-2] && isSorted(arr, n-1);
+}
+
 int main() {
     printNum(5);
     //Recursion
     cout << "Factorial of 5 is " << fact(5) << endl;
     cout << "Sum of first 5 natural numbers is " << Sum(5) << endl;
+    int arr[5] = {1,2,3,4,5};
+    cout << "Is Sorted: " << isSorted(arr, 5) << endl;
 
     return 0;
 }
