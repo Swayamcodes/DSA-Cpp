3a2ef6e3ef51cb54e8e26c9015b9648340ffc8c7
Swayam Shinde <shindeswayam2004@gmail.com>
2025-09-04 12:06:28 +0530

    Fix recursive function call in printNum and clean up factorial function implementation

--- 42.Recursion.cpp
+++ 42.Recursion.cpp
@@ -11,9 +11,17 @@ void printNum(int n) {
     func(n-1); //recursive call
 }
 
+int fact(int n) {
+    if ( n == 0 ) 
+        return 1;
+        
+        return n * fact(n-1);
+}
+
 int main() {
     printNum(5);
     //Recursion
+    cout << "Factorial of 5 is " << fact(5) << endl;a
 
     return 0;
 }
