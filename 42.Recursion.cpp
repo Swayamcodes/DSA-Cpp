b2e671564c4945a2f5a8ded288d5d146e843de95
Swayam Shinde <shindeswayam2004@gmail.com>
2025-09-10 22:18:47 +0530

    Add Merge Sort implementation and integrate into main function

--- 42.Recursion.cpp
+++ 42.Recursion.cpp
@@ -69,6 +69,54 @@ void printSubarrays2(vector<int> &arr, int n) {
     }
 }
 
+//Merge Sort 
+void Merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+    for ( int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+    for ( int j = 0; j < n2; j++) {
+        R[j] = arr[m + 1 + j];
+    }
+
+    int i = 0, j = 0, k = l;
+    while ( i < n1 && j < n2) {
+        if ( L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while ( i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while ( j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void MergeSort(int arr[], int l, int r) {
+    if ( l < r ) {
+        int m = l + (r - l) / 2;
+        MergeSort(arr, l, m);
+        MergeSort(arr, m + 1, r);
+        Merge(arr, l, m, r);
+    }
+}
+
+
 
 int main() {
     printNum(5);
@@ -81,10 +129,13 @@ int main() {
     vector<int> ans;
     printSubarrays(arr2, ans, 0);
 
+    MergeSort(arr, 0, 4);
+
     return 0;
 }
 
 // g++ -std=c++14 42.Recursion.cpp && ./a.out
+
 
 
 
