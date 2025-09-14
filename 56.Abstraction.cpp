e662a21a0e8acccbcc7604f2e3fbe7fdf0c0dce6
Swayam Shinde <shindeswayam2004@gmail.com>
2025-09-14 19:17:32 +0530

    Add abstraction example with Shape and Circle classes; implement static variable demonstration

---
+++ 56.Abstraction.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape { //abstract class
+    virtual void draw() = 0; //pure virtual function
+};
+
+class Circle : public Shape {
+    public :
+    void draw() {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+//Static keyword is used to allocate memory only once during the lifetime of a class
+//Static members are shared by all objects of the class
+
+void fun() { //function with static variable
+    static int count = 0; //init static variable only once
+    count++;
+    cout << "Count: " << count << endl;
+}
+
+int main() {
+    Circle c1;
+    c1.draw();
+    return 0;
+};
+
--- 56.Polymorphism.cpp
+++ 56.Polymorphism.cpp
@@ -14,11 +14,54 @@ class Student {
         cout << "Parameterized constructor called" << endl;
     }
 
-}
+};
+
+class Print { // function overloading
+    public :
+    void show(int x) {
+        cout << "Integer: " << x << endl;
+    }
+    void show(string str) {
+        cout << "String: " << str << endl;
+    }
+    void show(double d) {
+        cout << "Double: " << d << endl;
+    }
+
+};
+
+class Parent { // function overriding
+    public :
+    void getInfo() {
+        cout << "I am Parent class" << endl;
+    }
+
+    virtual void hello() {
+        cout << "Hello from Parent class" << endl;
+    }
+};
+class Child : public Parent {
+    public :
+    void getInfo() { //overriding
+        cout << "I am Child class" << endl;
+    }
+    void hello() override { //overriding
+        cout << "Hello from Child class" << endl;
+    }
+};
 
 int main() {
     Student s1; //non-parameterized constructor
     Student s2("Alice"); //parameterized constructor
+    Print p1;
+    p1.show(5);
+    p1.show("Hello");
+    p1.show(3.14);
+
+    Child c1;
+    c1.getInfo(); //Child class method (overriding)
+    c1.hello(); //Child class method (overriding) 
+
     return 0;
 
 }
