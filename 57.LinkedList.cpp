abac3e4d6fe55f1d4bc1ae26b853bb604694fe50
Swayam Shinde <shindeswayam2004@gmail.com>
2025-09-18 17:00:20 +0530

    Implement List class with push_front method for LinkedList

--- 57.LinkedList.cpp
+++ 57.LinkedList.cpp
@@ -9,5 +9,35 @@ class Node {
     Node(int val) {
         data = val;
         next = NULL;
+    };
+
+
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+    List()
+    {
+        head = NULL;
+        tail = NULL;
     }
-}
+
+    void push_front (int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL ) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head; //link new node to previous head
+            head = newNode;
+        }
+    }
+
+};
+
+int main() {
+    List ll;  
+    return 0;
+};
